039eaa5464c32b4f03a235e413370f9034f3a851
sammiemac <snv.macaraniag@gmail.com>
2022-10-29 23:55:23 -0700

    Revert "Revert "chp27-finished""
    This reverts commit 11c6afe070acd856eed24b7f87619509f00f2dc2.

--- app/src/main/java/com/sammiemac/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/sammiemac/android/photogallery/PhotoGalleryActivity.kt
@@ -1,5 +1,7 @@
 package com.sammiemac.android.photogallery
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -16,6 +18,12 @@ class PhotoGalleryActivity : AppCompatActivity() {
                 .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
                 .commit()
         }
-
     }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, PhotoGalleryActivity::class.java)
+        }
+    }
+
 }
--- app/src/main/java/com/sammiemac/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/sammiemac/android/photogallery/PhotoGalleryFragment.kt
@@ -15,9 +15,12 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.*
+import java.util.concurrent.TimeUnit
 
 
 private const val TAG = "PhotoGalleryFragment"
+private const val POLL_WORK = "POLL_WORK"
 
 class PhotoGalleryFragment : Fragment() {
 
@@ -42,6 +45,7 @@ class PhotoGalleryFragment : Fragment() {
             }
 
         lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
+
     }
 
     override fun onCreateView(
@@ -115,6 +119,15 @@ class PhotoGalleryFragment : Fragment() {
 
         }
 
+        val toggleItem = menu.findItem(R.id.menu_item_toggle_polling)
+        val isPolling = QueryPreferences.isPolling(requireContext())
+        val toggleItemTitle = if (isPolling) {
+            R.string.stop_polling
+        } else {
+            R.string.start_polling
+        }
+        toggleItem.setTitle(toggleItemTitle)
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -123,6 +136,31 @@ class PhotoGalleryFragment : Fragment() {
                 photoGalleryViewModel.fetchPhotos("")
                 true
             }
+
+            R.id.menu_item_toggle_polling -> {
+                val isPolling = QueryPreferences.isPolling(requireContext())
+                if (isPolling) {
+                    WorkManager.getInstance().cancelUniqueWork(POLL_WORK)
+                    QueryPreferences.setPolling(requireContext(), false)
+                } else {
+                    val constraints = Constraints.Builder()
+                        .setRequiredNetworkType(NetworkType.UNMETERED)
+                        .build()
+                    val periodicRequest = PeriodicWorkRequest
+                        .Builder(PollWorker::class.java, 15, TimeUnit.MINUTES)
+                        .setConstraints(constraints)
+                        .build()
+                    WorkManager.getInstance().enqueueUniquePeriodicWork(
+                        POLL_WORK,
+                        ExistingPeriodicWorkPolicy.KEEP,
+                        periodicRequest
+                    )
+                    QueryPreferences.setPolling(requireContext(), true)
+                }
+                activity?.invalidateOptionsMenu()
+                return true
+            }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
